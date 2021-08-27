4ac003c73ee42f688fba1d7ef3147161a28a4d80
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 14:30:22 +0530

    Create Program5.php

---
+++ Assignments/PHP/1/Program5.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * PHP program else if ladder statement 
+ *check between two numbers is less 
+than, is greater than, is equal to.
+ * By Piyush
+ */
+a = random_int(0, 10);
+$b = random_int(0, 10);
+if ($a > $b) {
+    echo "a is bigger than b";
+} elseif ($a == $b) {
+    echo "a is equal to b";
+} else {
+    echo "a is smaller than b";
+} 
+?>
