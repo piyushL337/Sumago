3d48ab215aeae5a439c4b39bd7cb261ba81e3dfb
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 16:45:38 +0530

    Create Program3.php

---
+++ Assignments/PHP/2/Program3.php
@@ -0,0 +1,19 @@
+<?php
+/*
+   *    PHP program using index array.
+     *
+       * By PIYUSH
+      *
+     *
+   *
+ */
+
+$friend = array("Piyush","Ajinkya","Dipak");
+
+echo count($friend);
+
+echo "Name Of Friends: $friend[0], $friend[1] and $friend[2]";
+
+?>
+
+  
