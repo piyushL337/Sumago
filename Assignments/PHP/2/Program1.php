b25f6b677d965333f50111c68cdb167c16429be2
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 16:32:49 +0530

    Create Program1.php

---
+++ Assignments/PHP/2/Program1.php
@@ -0,0 +1,27 @@
+<?php
+/*
+   *    PHP program using string manipulation.
+     *
+       * By PIYUSH
+      *
+     *
+   *
+ */
+$r= "i am Piyush";
+
+echo("$r");
+
+echo strlen("Hi Every One"); 
+// used strlen to find the length of the string 
+
+echo str_word_count("Hi Every One");
+//Used str_word_count To find the number of words
+ 
+echo strrev("Hello world");
+//Used to reverse a string
+
+echo strpos("Hi Buddy", "Buddy");
+
+echo str_replace("world", "jack", "Hello world");
+
+?>
