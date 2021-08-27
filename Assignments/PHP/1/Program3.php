f44e7b63fb575c67165f9be4390deeb2537b0cb4
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 11:03:34 +0530

    Create Program3.php

---
+++ Assignments/PHP/1/Program3.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * PHP program if statement
+ * Check Age Is Less Than 30
+ * By Piyush
+ */
+if(isset($_POST['check_age'])){
+  $age=$_POST['age'];
+  if($age<30){
+     echo '<p>age is less than 30</p>';
+  }
+  else{
+     echo '<p>age is over than 30</p>';
+  }
+}
