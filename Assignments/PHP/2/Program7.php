2e2ccf0e1fedac87bf0b228b81d23b8a7c60a424
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 17:02:41 +0530

    Create Program7.php

---
+++ Assignments/PHP/2/Program7.php
@@ -0,0 +1,21 @@
+<?php
+/*
+   *    PHP program using function with parameters.
+     *
+       * By PIYUSH
+      *
+     *
+   *
+ */
+
+function friendsName($frndname) {
+  echo "$frndname Refsnes.<br>";
+}
+
+friendsName("Ajinkya");
+friendsName("Pratik");
+friendsName("Omkar");
+friendsName("Kedar");
+friendsName("Vaibhav");
+
+?>
