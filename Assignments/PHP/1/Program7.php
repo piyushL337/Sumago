40ca428d7c314c3cc8460c462d7fedbb2bd661ef
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 14:41:43 +0530

    Create Program7.php

---
+++ Assignments/PHP/1/Program7.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * PHP program to display the numbers from 1 to 10 using while loop. 
+ *
+ * By Piyush
+ */
+$piyush = 1;
+
+while($piyush <= 10)
+{
+    echo "$piyush | ";
+    $piyush++;   // incrementing value of a by 1
+}
+?>
