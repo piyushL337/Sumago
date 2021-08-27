e7f699cada2b866219c1f13c8377cf4772c4e9d0
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 16:39:30 +0530

    Create Program2.php

---
+++ Assignments/PHP/2/Program2.php
@@ -0,0 +1,18 @@
+<?php
+/*
+   *    PHP program using string concatenation.
+     *
+       * By PIYUSH
+      *
+     *
+   *
+ */
+$variable_1 =  "Piyush";
+
+$variable_2 = "is a Web Developer";
+
+$variable_3 = "From India";
+
+echo $variable_1." ".$variable_2." ".$variable_3;
+
+?>
