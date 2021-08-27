e8d52aa752fa00c412e1a1a8d4d1e85f7f8df767
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 14:47:33 +0530

    Create Program9.php

---
+++ Assignments/PHP/1/Program9.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ *  PHP program print numbers from 1 to 10, 
+this time we will be using for 
+loop.
+ *
+ * By Piyush
+ */
+for($x=1; $x<=10; $x++)
+{
+ if($x< 10)
+ {
+ echo "$x-";
+ }
+ else
+  {
+ echo "$x"."\n";
+  }
+}
+?>
