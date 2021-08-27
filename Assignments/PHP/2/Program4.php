95194f26c858b9ecb8de618656d226a7a21cb636
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 16:50:55 +0530

    Create Program4.php

---
+++ Assignments/PHP/2/Program4.php
@@ -0,0 +1,19 @@
+<?php
+/*
+   *    PHP program using associative array.
+     *
+       * By PIYUSH
+      *
+     *
+   *
+ */
+$marks['Maths'] = "85";
+
+$marks['Science'] = "89";
+
+$marks['English'] = "78";
+
+echo "Obtained Marks in Maths = ".$marks['Maths'];
+
+
+?>
