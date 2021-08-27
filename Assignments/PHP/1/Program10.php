a911850956cde0b1cdcedfe00cba7ae3a6c46d1d
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-27 16:24:18 +0530

    Create Program10.php

---
+++ Assignments/PHP/1/Program10.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ *PHP program find out 
+odd and even elements from an array using for 
+each loop.
+ *
+ * By PIYUSH
+ */
+$odd  = array("1", "3", "5", "7", "9");
+$even = array("2", "4", "6", "8", "10");
+
+foreach ($odd as $value) {
+  echo "Odd No: <br> $value <br>";
+}
+foreach ($even as $value) {
+  echo "Even No: <br> $value <br>";
+}
+?>
